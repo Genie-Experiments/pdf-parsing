43403893e44aa63735aa0385f5634c576685d0c1
Muhammad Ali <muhammad.alinaeem@emumba.com>
2025-10-06 15:36:51 +0500

    modified config

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
     process_pdf_files(DATA_DIRECTORY, OUTPUT_DIRECTORY, DOLPHIN_SCRIPT, MODEL_PATH)
 
     # 2. Process all JSON files in results directory
-    process_all_json_files(OUTPUT_DIRECTORY, SEGMENTS_TO_REFINE, PROCESS_CODE_USING_LLM, PROCESS_FIGURES_USING_LLM)
+    #process_all_json_files(OUTPUT_DIRECTORY, SEGMENTS_TO_REFINE, PROCESS_CODE_USING_LLM, PROCESS_FIGURES_USING_LLM)
 
     # 3. Batch fix markdown section hierarchy using TOC JSON files
     batch_fix_markdown_sections(TOC_JSON_DIRECTORY, OUTPUT_DIRECTORY)
