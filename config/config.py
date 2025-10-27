258459c4b75fcb7ae4627dbe2ab7a38ffc4778e1
muhammad-ali-emumba <muhammad.alinaeem@emumba.com>
2025-10-27 13:11:26 +0500

    Removed html-to-markdown directory

--- config/config.py
+++ config/config.py
@@ -12,7 +12,7 @@ OUTPUT_DIRECTORY = "./temp_results"  # Output directory (maintains same structur
 DOLPHIN_SCRIPT = "./Dolphin/demo_page_hf.py" # Do not modify this line
 MODEL_PATH = "./Dolphin/hf_model" # Do not modify this line
 HIERARCHY_JSON_DIRECTORY = "./section_hierarchy_pdfs"  # Directory containing TOC JSON files with section hierarchy
-HTML_TO_MARKDOWN_DIR = "./post_processing/html-to-markdown"  # Directory containing html-to-markdown binaries
+HTML_TO_MARKDOWN_DIR = "./html-to-markdown"  # Directory containing html-to-markdown binaries
 
 RAW_PDF_TEXT_DIR = "./temp_raw_text"  # Directory to store raw extracted PDF texts
 
